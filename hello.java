b69dc9b4e94f83f020c4c79877331671775bbd20
Byabasaija <basaijapascal9@gmail.com>
2021-11-16 22:31:36 +0300

    say hello

---
+++ hello.java
@@ -0,0 +1,18 @@
+public class hello {
+ public static String name = "hello world";
+public static int x = 20;
+ public static void sayHello() {
+    System.out.println(name);
+ }
+    public static void main( String[] args) {
+        sayHello();
+        if (x==10) {
+            
+        } else {
+            System.out.println("the value of x is " + x);
+        }
+
+    }
+
+    
+}
